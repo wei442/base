5a461ff96982646a8e95d3d60c32ed8f10093773
weiyong <weiyong@ochain.fun>
2019-03-27 10:16:46 +0800

    redis

--- microservice-provider-redis/src/main/java/com/cloud/provider/redis/StartBoot.java
+++ microservice-provider-redis/src/main/java/com/cloud/provider/redis/StartBoot.java
@@ -45,43 +45,43 @@ public class StartBoot {
 
 	//最大连接数, 默认8个
 	@Value("${jedis.maxTotal}")
-	public String maxTotal;
+	public Integer maxTotal;
 
 	//最大空闲连接数
 	@Value("${jedis.maxIdle}")
-	public String maxIdle;
+	public Integer maxIdle;
 
 	//最小空闲连接数
 	@Value("${jedis.minIdle}")
-	public String minIdle;
+	public Integer minIdle;
 
 	//连接时的最大等待毫秒数
 	@Value("${jedis.maxWait}")
-	public String maxWait;
+	public Integer maxWait;
 
 	//在获取连接的时候检查有效性
 	@Value("${jedis.testOnBorrow}")
-	public String testOnBorrow;
+	public Boolean testOnBorrow;
 
 	//在return给pool时，是否提前进行validate操作
 	@Value("${jedis.testOnReturn}")
-	public String testOnReturn;
+	public Boolean testOnReturn;
 
 	//空闲时检查有效性
 	@Value("${jedis.testWhileIdle}")
-	public String testWhileIdle;
+	public Boolean testWhileIdle;
 
 	//一个对象至少停留在idle状态的最短时间，大于0
 	@Value("${jedis.minEvictableIdleTimeMillis}")
-	public String minEvictableIdleTimeMillis;
+	public Long minEvictableIdleTimeMillis;
 
 	@Value("${jedis.timeBetweenEvictionRunsMillis}")
 	//逐出扫描的时间间隔(毫秒) 如果为负数,则不运行逐出线程, 默认-1
-	public String timeBetweenEvictionRunsMillis;
+	public Long timeBetweenEvictionRunsMillis;
 
 	//表示idle object evitor每次扫描的最多的对象数
 	@Value("${jedis.numTestsPerEvictionRun}")
-	public String numTestsPerEvictionRun;
+	public Integer numTestsPerEvictionRun;
 
 	//超时时间
 	@Value("${jedis.timeout}")
@@ -127,18 +127,18 @@ public class StartBoot {
 	 */
 	@Bean
     public JedisSentinelPool getJedisSentinelPool() {
-    	GenericObjectPoolConfig<Object> config = new GenericObjectPoolConfig<Object>();
-    	if (StringUtils.isNumeric(maxTotal)) config.setMaxTotal(Integer.parseInt(maxTotal));
-    	if (StringUtils.isNumeric(maxIdle)) config.setMaxIdle(Integer.parseInt(maxIdle));
-    	if (StringUtils.isNumeric(minIdle)) config.setMinIdle(Integer.parseInt(minIdle));
-		if (StringUtils.isNumeric(maxWait)) config.setMaxWaitMillis(Integer.parseInt(maxWait));
-		if (StringUtils.isNumeric(minEvictableIdleTimeMillis)) config.setMinEvictableIdleTimeMillis(Long.parseLong(minEvictableIdleTimeMillis));
-		if (StringUtils.isNumeric(timeBetweenEvictionRunsMillis)) config.setTimeBetweenEvictionRunsMillis(Long.parseLong(timeBetweenEvictionRunsMillis));
-		if (StringUtils.isNumeric(numTestsPerEvictionRun)) config.setNumTestsPerEvictionRun(Integer.parseInt(numTestsPerEvictionRun));
+    	GenericObjectPoolConfig<?> config = new GenericObjectPoolConfig<>();
+    	if (maxTotal != null) config.setMaxTotal(maxTotal);
+    	if (maxIdle != null) config.setMaxIdle(maxIdle);
+    	if (minIdle != null) config.setMinIdle(minIdle);
+    	if (maxWait != null) config.setMaxWaitMillis(maxWait);
+    	if (minEvictableIdleTimeMillis != null) config.setMinEvictableIdleTimeMillis(minEvictableIdleTimeMillis);
+    	if (timeBetweenEvictionRunsMillis != null) config.setTimeBetweenEvictionRunsMillis(timeBetweenEvictionRunsMillis);
+    	if (numTestsPerEvictionRun != null) config.setNumTestsPerEvictionRun(numTestsPerEvictionRun);
 
-		if ("true".equals(testOnBorrow) || "false".equals(testOnBorrow)) config.setTestOnBorrow(Boolean.parseBoolean(testOnBorrow));
-		if ("true".equals(testOnReturn) || "false".equals(testOnReturn)) config.setTestOnReturn(Boolean.parseBoolean(testOnReturn));
-		if ("true".equals(testWhileIdle) || "false".equals(testWhileIdle)) config.setTestWhileIdle(Boolean.parseBoolean(testWhileIdle));
+		if (testOnBorrow != null) config.setTestOnBorrow(testOnBorrow);
+		if (testOnReturn != null) config.setTestOnReturn(testOnReturn);
+		if (testWhileIdle != null) config.setTestWhileIdle(testWhileIdle);
 
 		Set<String> sentinels = new HashSet<String>();
 		String[] sentinelIps = StringUtils.split(sentinelIp, ",");
